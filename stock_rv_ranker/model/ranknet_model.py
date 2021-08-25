e1a8be0f0692fda8c69a12e26e7c961a09d4dc2e
Allen Chiang <49981062+allen-chiang@users.noreply.github.com>
2021-08-25 16:36:15 +0800

    update

--- stock_rv_ranker/model/ranknet_model.py
+++ stock_rv_ranker/model/ranknet_model.py
@@ -23,4 +23,33 @@ class RankNet_Lambda_Model(tf.keras.Model):
         return super().train_step(data)
 
 
-    
+    def apply_gradient_lambdarank(optimizer, model, x, score, mask, doc_cnt, eval_ndcg):
+        with tf.GradientTape() as tape:
+            oi = model(x)
+        
+        S_ij = tf.maximum(tf.minimum(tf.subtract(tf.expand_dims(score,1), score),1.),-1.)
+        P_ij = tf.multiply(mask, tf.multiply(0.5, tf.add(1., S_ij)))
+        P_ij_pred = tf.multiply(mask,tf.nn.sigmoid(tf.subtract(oi, tf.transpose(oi))))
+        lambda_ij = tf.add(tf.negative(P_ij), P_ij_pred)
+        
+        ndcg, ndcg_delta_ij = eval_ndcg(score, tf.squeeze(oi, 1), return_ndcg_delta=True)
+        lambda_ij = tf.multiply(lambda_ij, ndcg_delta_ij)
+        
+        lambda_i = tf.reduce_sum(lambda_ij,1) - tf.reduce_sum(lambda_ij,0)
+        
+        doi_dwk = tape.jacobian(oi, model.trainable_weights)
+        
+        # 1. reshape lambda_i to match the rank of the corresponding doi_dwk
+        # 2. multiple reshaped lambda_i with the corresponding doi_dwk
+        # 3. compute the sum across first 2 dimensions
+        gradients = list(map(lambda k: 
+                            tf.reduce_sum(tf.multiply(tf.reshape(lambda_i,  tf.concat([tf.shape(lambda_i),tf.ones(tf.rank(k) - 1, dtype=tf.int32)], axis=-1)), k), [0,1]),
+                            doi_dwk))
+        
+        # model could still be trained without calculating the loss below
+        loss_value = tf.reduce_sum(tf.keras.losses.binary_crossentropy(P_ij, P_ij_pred))
+        loss_value = tf.multiply(loss_value, doc_cnt)
+        
+        optimizer.apply_gradients(zip(gradients, model.trainable_weights))
+        
+        return oi, loss_value, ndcg
--- stock_rv_ranker/utils/preprocess.py
+++ stock_rv_ranker/utils/preprocess.py
@@ -1,13 +1,28 @@
 import numpy as np
 import pandas as pd
+from itertools import combinations
 
 
 
+def rank_to_score(data,symbolList,featureList):
+    xi = []
+    xj = []
+    y = []
+    indexList = []
+    for i in list(combinations(symbolList,2)):
+        xi.append([data[i[0]][j] for j in featureList])
+        xj.append([data[i[1]][j] for j in featureList])
+        y.append(1 if data[i[0]]['profit'] > data[i[1]]['profit'] else 0)
+        indexList.append(i)
+    return xi,xj,y,indexList
 
-def from_rank_to_score(rankArray:np.array,scale):
-    pass
 
+def score_to_rank(symbolList,indexList,prd):
+    res = {}
+    for i in symbolList:
+        res[i] = 0
+    for ix,p in zip(indexList,prd):
+        res[ix[0]] += p.reshape(-1)
+        res[ix[1]] += 1 - p.reshape(-1)
+    return pd.DataFrame.from_dict(res,'index').sort_values(0)
 
-
-def from_score_to_rank(scoreArray):
-    pass
